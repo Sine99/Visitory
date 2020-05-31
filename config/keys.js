c2d9db7edddc9b65de096c0cbdd2bcbd8c914d0b
Sine99 <saurav9920@gmail.com>
2020-06-01 03:25:43 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI : 'mongodb://team:hello@ds121192.mlab.com:21192/visitory-dev',
-    googleClientID : "521206552780-qqjd3u7aab32rgre4te7678jn00m104s.apps.googleusercontent.com",
-    googleClientSecret : 'SNiBfNky8qBU-32H6oFIGyAc'
-}
